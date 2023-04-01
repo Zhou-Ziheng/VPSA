705fd1e6abd303d557ff03f16929818041b94349
SparkyFnay <z1362110538@gmail.com>
2023-04-01 19:46:48 -0400

    sus

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,11 +2,14 @@ import { Box, Icon, TextField, InputAdornment } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import styles from "./home.module.scss";
 import { useState } from "react";
+import Image from "next/image";
+import SageBackground from "../../public/background.png";
 
 const Home = () => {
   const [searchInput, setSearchInput] = useState("");
   return (
     <div className={styles["home-background"]}>
+      <div className={styles["sage-div"]}></div>
       <div className={styles["text"]}>
         <h1 className={styles["title"]}>VALORANT POCKET SAGE ASSOCIATION</h1>
       </div>
