20ce5e22f9ddc91808bbacd3c5b141eb982f0a41
SparkyFnay <z1362110538@gmail.com>
2023-04-02 01:52:26 -0400

    saus

--- frontend/src/pages/resources.tsx
+++ frontend/src/pages/resources.tsx
@@ -1,9 +1,13 @@
-import { Typography } from "@mui/material";
+import { Typography, TextField } from "@mui/material";
 import TextPage from "../components/TextPage";
 
 const About = () => (
   <TextPage>
     <Typography variant="h3">Resources</Typography>
+    <TextField variant="outlined" />
+    <TextField variant="filled" />
+    <TextField variant="standard" />
+    <input style={{ color: "red", backgroundColor: "purple" }}></input>
   </TextPage>
 );
 export default About;
