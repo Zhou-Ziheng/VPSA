368f0a34b3dc5520a7b748e7f33cd5d6e12e5a47
Kitty Cai <kittychoi15@gmail.com>
2023-04-02 00:44:04 -0500

    Adjustments to Frontend

---
+++ frontend/src/pages/resources.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+import TextPage from "../components/TextPage";
+
+const About = () => (
+  <TextPage>
+    <Typography variant="h3">Resources</Typography>
+  </TextPage>
+);
+export default About;
