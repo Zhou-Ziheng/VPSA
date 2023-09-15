91d5f6ca151d0ff83cc3023b94a967ae0e784525
Tony Zhou <75397821+Zhou-Ziheng@users.noreply.github.com>
2023-09-15 00:27:44 -0400

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.scss";
 const Footer = () => (
   <div className={styles.footer}>
     <div className={styles.disclamer}>
-      VPSAA is not endorsed by Riot Games and does not reflect the views or
+      VPSAAA is not endorsed by Riot Games and does not reflect the views or
       opinions of Riot Games or anyone officially involved in producing or
       managing Riot Games properties. Riot Games and all associated properties
       are trademarks or registered trademarks of Riot Games, Inc
