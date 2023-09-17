52c50ac1be4fca2ab889ccb496dc3dae5e239d71
Paudzy <sampaudel0@gmail.com>
2023-09-17 00:20:41 -0400

    Update about.tsx
    Add a more detailed explanation to what is a pocket sage

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -8,22 +8,42 @@ const About = () => (
     </Typography>
     <Typography variant="h3">What is a Pocket Sage?</Typography>
     <Typography>
-      In Valorant, a pocket Sage is a player who offers support and guidance to
-      their team. Certification programs for Valorant pocket Sages are important
-      because they help ensure that players offering guidance and support have
-      the necessary skills and knowledge.
+      Valorant exists as an ever-evolving battleground, where each match is a canvas for strategic brilliance, teamwork, and individual prowess. Within this intricate 
+      tapestry, the role of a pocket Sage transcends mere in-game mechanics; it embodies the very essence of support, mentorship, and tactical acumen. These players are 
+      not just gunners; they are orchestrators of victory, guiding their teams through complex tactical scenarios. For them, success hinges not only on precise aiming 
+      but also on a profound understanding of the game's mechanics, strategy, and psychology. This is precisely where certification programs for Valorant pocket Sages 
+      step into the limelight, offering a structured pathway to excellence.
+     </Typography>
+     <Typography>
+      Certification programs aren't just a matter of acquiring a badge or a title; they represent an unwavering commitment to professionalism and accountability 
+      within the Valorant community. In an arena where competition is nothing short of fierce and every player's contribution has far-reaching consequences, it becomes 
+      imperative to ensure that those who shoulder the mantle of a pocket Sage are not just adept at aiming and shooting but also steeped in the virtues of ethics and 
+      responsibility. These programs function as beacons of enlightenment, guiding aspiring pocket Sages through a rigorous curriculum that not only refines their 
+      in-game skills but also molds them into upstanding members of the gaming fraternity.
+     </Typography>
+     <Typography>
+      The true significance of certification programs is not limited to individual growth; they are the bedrock upon which Valorant's integrity is built. By establishing 
+      clear-cut standards of proficiency and ethical conduct, these programs act as formidable bulwarks against the treacherous pitfalls that unqualified or unethical 
+      practitioners can present. Imagine, for a moment, a team that entrusts its destiny to a pocket Sage, only to discover that their guidance is misinformed or, worse 
+      yet, maliciously motivated. Certification programs are the custodians of fairness, protecting teams from such calamities and preserving the very essence of 
+      sportsmanship that underpins competitive gaming.
+     </Typography>
+     <Typography>
+      However, the impact of certification programs doesn't end there; it reverberates throughout the entire Valorant ecosystem. As pocket Sages undergo comprehensive 
+      training and certification, their influence on gameplay becomes increasingly profound. They metamorphose into the architects of success, weaving intricate strategies 
+      and tactics that can spell the difference between victory and defeat. Yet, it's not just about their tactical prowess; it's about the intangible quality of leadership 
+      and teamwork they bring to the table. Teams experience not only a surge in strategic insights but also an upswing in cohesion and coordination, leading to an overall 
+      elevation in the quality of matches.
+     </Typography>
+     <Typography>
+     In essence, certification programs for Valorant pocket Sages are far from being superficial rituals; they are the lifeblood that sustains professionalism, ensures ethical 
+     conduct, and elevates the gameplay experience for all. By investing in these programs, the Valorant community is making a profound statement about its unwavering 
+     commitment to excellence. Whether you aspire to become a certified pocket Sage, are privileged to collaborate with one, or simply recognize the invaluable contributions 
+     these players make to the Valorant tapestry, these programs stand as a testament to a collective aspiration for Valorant to flourish as a competitive, enjoyable, and 
+     ethically sound gaming platform. In embracing their significance, we chart a course for Valorant's future where it continues to shine as a beacon of integrity and 
+     exhilarating gameplay for players of diverse backgrounds and aspirations.
     </Typography>
     <Typography>
-      By requiring certification, these programs promote professionalism and
-      accountability within the Valorant community. They help to establish
-      standards of competence and ethical conduct for pocket Sages, which in
-      turn helps to protect teams from unqualified or unethical practitioners.
-    </Typography>
-    <Typography>
-      Certification programs for Valorant pocket Sages also help to improve the
-      overall quality of play in the game. By promoting the development of
-      skilled and knowledgeable pocket Sages, teams can perform better and
-      achieve greater success.
     </Typography>
   </TextPage>
 );
