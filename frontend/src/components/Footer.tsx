d0ae74852287f1ab351d254d59d5dd4569594e13
nexuspy <59784880+nexuspy@users.noreply.github.com>
2023-09-20 22:51:14 +0530

    changes made in footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -7,13 +7,13 @@ const discordLink = "https://discord.gg/rpNkB2kwyb"; // Discord invite link
 const Footer = () => (
   <div className={styles.footer}>
     <div className={styles.disclamer}>
-      VPSAAA is not endorsed by Riot Games and does not reflect the views or
+      VPSA is not endorsed by Riot Games and does not reflect the views or
       opinions of Riot Games or anyone officially involved in producing or
       managing Riot Games properties. Riot Games and all associated properties
       are trademarks or registered trademarks of Riot Games, Inc
     </div>
     <a href={discordLink} target="_blank" rel="noopener noreferrer">
-      <Image src={DiscordIcon} alt="Discord" width={30} height={50}/>
+      <Image src={DiscordIcon} alt="Discord" width={30} height={50} />
     </a>
   </div>
 );
