296e90185e913bab2ca3baa87f3132eb07081f90
SparkyFnay <z1362110538@gmail.com>
2023-09-22 17:28:56 -0400

    added privacy and tos page

---
+++ frontend/src/pages/privacy.tsx
@@ -0,0 +1,62 @@
+import { Typography, TextField } from "@mui/material";
+import TextPage from "../components/TextPage";
+
+const About = () => (
+  <TextPage>
+    <h3>Privacy Policy</h3>
+
+    <p>
+      Your privacy is important to us. This Privacy Policy outlines the types of
+      personal information that is received and collected and how it is used.
+    </p>
+
+    <h6>Information We Collect</h6>
+
+    <p>
+      We may collect personal information such as your name, email address, and
+      other contact details when you fill out a form on our website or otherwise
+      provide it to us.
+    </p>
+
+    <h6>How We Use Your Information</h6>
+
+    <p>
+      We use the information we collect to communicate with you and provide the
+      services you request. We may also use your information to send promotional
+      emails or newsletters.
+    </p>
+
+    <h6>Sharing of Information</h6>
+
+    <p>
+      We do not sell, rent, or trade your personal information with third
+      parties. We may share your information with trusted third parties who
+      assist us in operating our website, conducting our business, or servicing
+      you.
+    </p>
+
+    <h6>Security</h6>
+
+    <p>
+      We take appropriate measures to protect your personal information from
+      unauthorized access or disclosure. However, no data transmission over the
+      internet or any wireless network can be guaranteed to be 100% secure.
+    </p>
+
+    <h6>Changes to this Privacy Policy</h6>
+
+    <p>
+      We may update this Privacy Policy from time to time. We encourage you to
+      review this page periodically for the latest information on our privacy
+      practices.
+    </p>
+
+    <h6>Contact Us</h6>
+
+    <p>
+      If you have any questions or concerns about our Privacy Policy, please
+      contact us on GitHub.
+    </p>
+  </TextPage>
+);
+export default About;
