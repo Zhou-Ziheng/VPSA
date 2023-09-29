b2b130f76ac4c1c3bba098e9f0198ec918645252
SparkyFnay <z1362110538@gmail.com>
2023-09-29 19:34:47 -0400

    interesting

---
+++ frontend/src/pages/redirect.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { encode } from "querystring";
+
+const Redirect = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { query } = router;
+    const referrerUrl = query?.referrer_url;
+
+    if (!referrerUrl) {
+      router.push("/");
+      return;
+    }
+
+    // Remove the 'referrer_url' parameter from the query object
+    delete query.referrer_url;
+
+    // Construct the final redirect URL with remaining query parameters
+    const queryString = new URLSearchParams(encode(query));
+    const redirectUrl = `${referrerUrl}?${queryString}`;
+
+    // Redirect the user to the referral URL with the additional query parameters
+    window.location.href = redirectUrl;
+  }, [router]);
+
+  return <div>Redirecting...</div>;
+};
+
+export default Redirect;
