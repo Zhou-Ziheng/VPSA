d2ccb00acb6392c36d3a8f88c5f336adb4a6ed6d
Tony Zhou <75397821+Zhou-Ziheng@users.noreply.github.com>
2023-09-29 20:51:57 -0400

    Update redirect.tsx

--- frontend/src/pages/redirect.tsx
+++ frontend/src/pages/redirect.tsx
@@ -10,7 +10,7 @@ const Redirect = () => {
 
     // Construct the final redirect URL with remaining query parameters
     const queryString = new URLSearchParams(encode(query));
-    const redirectUrl = `http://localhost:3000?${queryString}`;
+    const redirectUrl = `http://localhost:3000/oauth/callback?${queryString}`;
 
     // Redirect the user to the referral URL with the additional query parameters
     window.location.href = redirectUrl;
